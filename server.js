20e4a9ed7ea84baef212e5dccd78df6d0ea6ea26
Aleksandr Maniunin <aamanunin@gmail.com>
2019-09-25 21:42:56 +0700

    add user model

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+// connect to DB
+connectDB();
+
+// init Middleware
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => res.json({ msg: 'Welcome to Contact Keeper...' }));
 
